f8dde9950a7c85dcb6e5b450cef8a9b40d4a7baf
VISHAL <vishal93ranjan@gmail.com>
2025-06-28 20:46:46 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return "<h1>Hello from FastAPI on Render!</h1>"
+
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
